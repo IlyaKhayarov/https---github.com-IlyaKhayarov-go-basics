fc63f1db9e535b97f63c5cf552fa62d032065111
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2024-07-17 01:17:46 +0300

    Package 2

--- Go/go-basics/Package/Main.go
+++ Go/go-basics/Package/Main.go
@@ -6,6 +6,14 @@ import (
 )
 
 func main() {
-	testSlice := []int{2, 4}
-	fmt.Println(mathslice.SumSlice(testSlice))
+	testSlice := mathslice.Slice{2, 4}
+	fmt.Printf("Сумма слайса %v: = %d\n", testSlice, mathslice.SumSlice(testSlice))
+
+	testVal := 3
+
+	fmt.Printf("Умножение слайса %v на %v\n", testSlice, testVal)
+	mathslice.MapSlice(testSlice, testVal, func(i mathslice.El, val int) mathslice.El {
+		return i * mathslice.El(testVal)
+	})
+	fmt.Printf("Равно %v\n", testSlice)
 }
--- Go/go-basics/Package/MathSlice/MathSlice.go
+++ Go/go-basics/Package/MathSlice/MathSlice.go
@@ -1,9 +1,17 @@
 package mathslice
 
-func SumSlice(s []int) int {
-	var res int
+type Slice []El
+type El int
+
+func SumSlice(s Slice) (res El) {
 	for _, v := range s {
 		res += v
 	}
 	return res
+}
+
+func MapSlice(s Slice, val int, op func(El, int) El) {
+	for i, v := range s {
+		s[i] = op(v, val)
+	}
 }
