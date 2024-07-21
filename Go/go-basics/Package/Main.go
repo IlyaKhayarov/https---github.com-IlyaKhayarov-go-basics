838a7f414c043d742817a299746c7e1ed1518bd4
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2024-07-22 00:55:43 +0300

    Package 3

--- Go/go-basics/Package/Main.go
+++ Go/go-basics/Package/Main.go
@@ -6,14 +6,23 @@ import (
 )
 
 func main() {
-	testSlice := mathslice.Slice{2, 4}
+	testSlice := mathslice.Slice{2, 4, 3}
 	fmt.Printf("Сумма слайса %v: = %d\n", testSlice, mathslice.SumSlice(testSlice))
 
 	testVal := 3
+	copiedSlice := make([]mathslice.El, len(testSlice))
+	copy(copiedSlice, testSlice)
 
-	fmt.Printf("Умножение слайса %v на %v\n", testSlice, testVal)
+	fmt.Printf("Умножение слайса %v на %v ", testSlice, testVal)
 	mathslice.MapSlice(testSlice, testVal, func(i mathslice.El, val int) mathslice.El {
 		return i * mathslice.El(testVal)
 	})
 	fmt.Printf("Равно %v\n", testSlice)
+
+	fmt.Printf("Свёртка слайса %v умножением = %v\n", copiedSlice, mathslice.FoldSlice(copiedSlice, func(a mathslice.El, b mathslice.El) mathslice.El {
+		return a * b
+	}, 1))
+	fmt.Printf("Свёртка слайса %v сложением = %v\n", copiedSlice, mathslice.FoldSlice(copiedSlice, func(a mathslice.El, b mathslice.El) mathslice.El {
+		return a + b
+	}, 0))
 }
--- Go/go-basics/Package/MathSlice/MathSlice.go
+++ Go/go-basics/Package/MathSlice/MathSlice.go
@@ -15,3 +15,15 @@ func MapSlice(s Slice, val int, op func(El, int) El) {
 		s[i] = op(v, val)
 	}
 }
+
+func FoldSlice(s Slice, op func(El, El) El, init El) (res El) {
+	for i, v := range s {
+		switch i {
+		case 0:
+			res = op(init, s[0])
+		default:
+			res = op(res, v)
+		}
+	}
+	return res
+}
